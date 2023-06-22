481d30dfd34c1e8b4fad5fc1ccbf1f1f48d82230
ShunPeng1 <91878382+ShunPeng1@users.noreply.github.com>
2023-06-22 18:15:09 +0700

    Initial commit

---
+++ Assets/Collections/StateMachine/StateMachine.cs
@@ -0,0 +1,126 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+namespace Collections.StateMachine
+{
+    public abstract class StateMachine<TStateEnum> : MonoBehaviour where TStateEnum : Enum
+    {
+        [Header("State Machine ")]
+        [SerializeField] public TStateEnum _myStateEnum;
+
+        protected List<Func<TStateEnum,object[],IEnumerator>> onEnterEvents = new();
+        protected List<Func<TStateEnum,object[],IEnumerator>> onExitEvents = new();
+    
+        
+        public enum StateEvent
+        {
+            OnEnter,
+            OnExit
+        }
+
+        public IEnumerator OnExitState(TStateEnum enterState = default, object [] parameters = null)
+        {
+            foreach (var exitEnumerator in onExitEvents)
+            {
+                yield return StartCoroutine(exitEnumerator.Invoke(enterState, parameters));
+            }
+        }
+        
+        public IEnumerator OnEnterState(TStateEnum exitState = default, object [] parameters = null)
+        {
+            foreach (var enterEnumerator in onEnterEvents)
+            {
+                yield return StartCoroutine(enterEnumerator.Invoke(exitState, parameters));
+            }
+        }
+
+    }
+    
+    public class StateMachine<TMonoBehavior, TStateEnum> : StateMachine<TStateEnum>
+        where TMonoBehavior : StateMachine<TStateEnum>
+        where TStateEnum : Enum 
+    {
+        public void AddToFunctionQueue(Action<TStateEnum> action, StateEvent stateEvent)
+        {
+            switch (stateEvent)
+            {
+                case StateEvent.OnEnter:
+                    onEnterEvents.Add((stateEnum,_) => ConvertToIEnumerator(action, stateEnum));
+                    break;
+                case StateEvent.OnExit:
+                    onExitEvents.Add((stateEnum,_) => ConvertToIEnumerator(action, stateEnum));
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(stateEvent), stateEvent, null);
+            }
+        
+        }
+        
+        public void AddToFunctionQueue(Action<TStateEnum, object[]> action, StateEvent stateEvent)
+        {
+            switch (stateEvent)
+            {
+                case StateEvent.OnEnter:
+                    onEnterEvents.Add((stateEnum, parameters) => ConvertToIEnumerator(action, stateEnum, parameters));
+                    break;
+                case StateEvent.OnExit:
+                    onExitEvents.Add((stateEnum, parameters) => ConvertToIEnumerator(action, stateEnum, parameters));
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(stateEvent), stateEvent, null);
+            }
+        
+        }
+        
+        public void AddToFunctionQueue(Func<TStateEnum,object[], IEnumerator> coroutine, StateEvent stateEvent)
+        {
+            switch (stateEvent)
+            {
+                case StateEvent.OnEnter:
+                    onEnterEvents.Add(coroutine);
+                    break;
+                case StateEvent.OnExit:
+                    onExitEvents.Add(coroutine);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(stateEvent), stateEvent, null);
+            }
+        }
+
+        public void AddToFunctionQueue(Tween tween, StateEvent stateEvent)
+        {
+            switch (stateEvent)
+            {
+                case StateEvent.OnEnter:
+                    onEnterEvents.Add((_,_) => ConvertToIEnumerator(tween));
+                    break;
+                case StateEvent.OnExit:
+                    onExitEvents.Add((_,_) => ConvertToIEnumerator(tween));
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(stateEvent), stateEvent, null);
+            }
+        }
+        
+        private IEnumerator ConvertToIEnumerator(Action<TStateEnum> action, TStateEnum stateEnum)
+        {
+            action.Invoke(stateEnum);
+            yield return null;
+        }
+        
+        private IEnumerator ConvertToIEnumerator(Action<TStateEnum, object[]> action, TStateEnum stateEnum, object[] parameters = null)
+        {
+            action.Invoke(stateEnum, parameters);
+            yield return null;
+        }
+        
+        private IEnumerator ConvertToIEnumerator(Tween tween)
+        {   
+            yield return tween.WaitForCompletion(); //Intentionally make them to Coroutine
+        }
+
+    }
+}
---
+++ Assets/Collections/StateMachine/StateMachineManager.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityUtilities;
+
+namespace Collections.StateMachine
+{
+    public class StateMachineManager<TMonoBehaviour,TStateEnum> : SingletonMonoBehaviour<TMonoBehaviour> where TStateEnum : Enum where TMonoBehaviour : SingletonMonoBehaviour<TMonoBehaviour>
+    {
+        protected StateMachine<TStateEnum> currentStateMachine;
+        private Dictionary<TStateEnum, StateMachine<TStateEnum>> _states = new ();
+
+        private Queue<(StateMachine<TStateEnum>, object[], object[])> _changingStateQueue = new ();
+
+        [Header("History")] 
+        [SerializeField, Range(0,20)]private int _maxHistoryLength = 10;
+        private LinkedList<(StateMachine<TStateEnum>, object[], object[])> _historyStates = new();
+        
+        private bool _isChangingState;
+        
+        public void AddState(TStateEnum stateEnum, StateMachine<TStateEnum> stateMachine)
+        {
+            _states[stateEnum] = stateMachine;
+        }
+
+        public void RemoveState(TStateEnum stateEnum)
+        {
+            _states.Remove(stateEnum);
+        }
+
+        public void SetState(TStateEnum stateEnum, object[] exitParameters = null, object[] enterParameters = null)
+        {
+            if (_states.TryGetValue(stateEnum, out StateMachine<TStateEnum> nextState))
+            {
+                //Debug.Log("State Machine Manager Enqueue state "+ nextState);
+                _changingStateQueue.Enqueue((nextState, exitParameters, enterParameters));
+                StartCoroutine(nameof(SwitchingState));
+                AddStateToHistory(nextState, exitParameters, enterParameters);
+            }
+            else
+            {
+                Debug.LogWarning($"State {stateEnum} not found in state machine.");
+            }
+        }
+        
+        public void SetToLastState()
+        {
+            if (_historyStates.Count != 0)
+            {
+                _historyStates.RemoveFirst();
+                _changingStateQueue.Enqueue(_historyStates.First.Value);
+                StartCoroutine(nameof(SwitchingState));
+            }
+            else
+            {
+                Debug.LogError("No state in the history state machine");
+            }
+            
+        }
+
+        private void AddStateToHistory( StateMachine<TStateEnum> state, object[] exitParameters = null, object[] enterParameters = null)
+        {
+            if (_historyStates.Count >= _maxHistoryLength)
+            {
+                _historyStates.RemoveLast();
+            }
+
+            _historyStates.AddFirst((state, exitParameters, enterParameters));
+        }
+        
+        public TStateEnum GetState()
+        {
+            return currentStateMachine._myStateEnum;
+        }
+
+        private IEnumerator SwitchingState()
+        {
+            if (_isChangingState)
+            {
+                yield break;
+            }
+
+            _isChangingState = true;
+            while (_changingStateQueue.Count>0)
+            {
+                var info = _changingStateQueue.Dequeue();
+                var nextState = info.Item1;
+                object[] exitParameters = info.Item2, enterParameters = info.Item3 ;
+                Debug.Log("State Machine Manager Change"+ currentStateMachine+ " To "+ nextState);
+
+                yield return StartCoroutine(currentStateMachine.OnExitState(nextState._myStateEnum,exitParameters));
+                yield return StartCoroutine(nextState.OnEnterState(currentStateMachine._myStateEnum,enterParameters));
+                currentStateMachine = nextState;
+            }
+
+            _isChangingState = false;
+        }
+        
+    }
+}
