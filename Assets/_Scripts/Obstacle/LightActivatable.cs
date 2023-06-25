6bd50a58d0aa6e5e3662cb657ceb7e1499344209
ShunPeng1 <91878382+ShunPeng1@users.noreply.github.com>
2023-06-25 15:41:46 +0700

    feat: light turn on off

--- Assets/_Scripts/Obstacle/LightActivatable.cs
+++ Assets/_Scripts/Obstacle/LightActivatable.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using _Scripts.Obstacle;
 using CodeMonkey.Utils;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 
@@ -12,6 +13,11 @@ public class LightActivatable : Activatable
     [SerializeField] private LayerMask _hitLayerMask, _triggerLayerMask;
     private PolygonCollider2D _polygonCollider2D;
     private Light2D _light2D;
+    
+    [Header("Tween")]
+    [SerializeField] private float _turnOnDuration = 0.3f, _turnOffDuration = 0.15f;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -56,12 +62,16 @@ public class LightActivatable : Activatable
 
     protected override void Active()
     {
-        gameObject.SetActive(true);
+        _light2D.intensity = 0;
+        DOTween.To(() => _light2D.intensity, x => _light2D.intensity = x, 1, _turnOnDuration);
+        _polygonCollider2D.enabled = true;
     }
 
     protected override void Inactive()
     {
-        gameObject.SetActive(false);
+        _light2D.intensity = 1;
+        DOTween.To(() => _light2D.intensity, x => _light2D.intensity = x, 0, _turnOnDuration);
+        _polygonCollider2D.enabled = false;
     }
 
     public void OnTriggerEnter2D(Collider2D other)
